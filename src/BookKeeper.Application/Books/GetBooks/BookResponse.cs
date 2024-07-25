ab9ed288267931c2cda3053135c102f4f90b256d
cnrdvy@gmail.com <cnrdvy@gmail.com>
2024-07-25 23:33:25 +1000

    Switched to parameter based constructor Added empty constructor

--- src/BookKeeper.Application/Books/GetBooks/BookResponse.cs
+++ src/BookKeeper.Application/Books/GetBooks/BookResponse.cs
@@ -1,8 +1,30 @@
 ﻿namespace BookKeeper.Application.Books.GetBooks;
 
-public sealed record BookResponse(
-    Guid Id,
-    string Title,
-    string Description,
-    string Authors,
-    decimal Price);
+public sealed record BookResponse
+{
+    public BookResponse(
+        Guid Id,
+        string Title,
+        string Description,
+        IEnumerable<Guid> Authors,
+        decimal Price)
+    {
+        this.Id = Id;
+        this.Title = Title;
+        this.Description = Description;
+        this.Authors = Authors;
+        this.Price = Price;
+    }
+
+    public BookResponse() { }
+
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public IEnumerable<Guid> Authors { get; set; }
+
+    public decimal Price { get; set; }
+}
