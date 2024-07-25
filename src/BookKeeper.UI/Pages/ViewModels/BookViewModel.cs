c6ac7819602e6d158e920e51a435ef5267ae1c11
cnrdvy@gmail.com <cnrdvy@gmail.com>
2024-07-25 23:36:26 +1000

    Updated ViewModels

--- src/BookKeeper.UI/Pages/ViewModels/BookViewModel.cs
+++ src/BookKeeper.UI/Pages/ViewModels/BookViewModel.cs
@@ -1,7 +1,23 @@
-﻿namespace BookKeeper.UI.Pages.Admin;
+﻿using System.ComponentModel.DataAnnotations;
+using BookKeeper.Domain.Entities;
 
-public sealed record BookViewModel(
-    string Title, 
-    string Description, 
-    string Authors, 
-    decimal Price);
+namespace BookKeeper.UI.Pages.Admin;
+
+public sealed record BookViewModel
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public IEnumerable<Author> Authors { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = ValidationErrorMessages.NumberMustBeGreaterThanZero)]
+    public decimal Price { get; set; }
+
+    internal static class ValidationErrorMessages
+    {
+        public const string NumberMustBeGreaterThanZero = "The Price field must be greater than 0.";
+    }
+}
--- src/BookKeeper.UI/Pages/ViewModels/UserViewModel.cs
+++ src/BookKeeper.UI/Pages/ViewModels/UserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using BookKeeper.Domain.ValueObjects;
 
 namespace BookKeeper.UI.Pages.ViewModels;
 
