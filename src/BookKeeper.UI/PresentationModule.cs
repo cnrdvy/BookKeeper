ec3633df12d461bf8e41f32ed980310c20225dc4
cnrdvy@gmail.com <cnrdvy@gmail.com>
2024-08-11 20:11:46 +1000

    -UPD: Extract Presentation dependency setup from program.cs

---
+++ src/BookKeeper.UI/PresentationModule.cs
@@ -0,0 +1,12 @@
+﻿namespace BookKeeper.UI;
+
+public static class PresentationModule
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddAutoMapper(BookKeeper.UI.AssemblyReference.Assembly);
+        
+        return services; 
+    }
+}
